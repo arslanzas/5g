55474c4cb57fc0f5b7613d41964a478d7db0af2c
arslanzas <arslanzas@gmail.com>
2026-09-14 16:01:47 +0500

    Add structureTool plugin to Sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
 import { schema } from './sanity/schema'
 
 export default defineConfig({
@@ -6,5 +7,6 @@ export default defineConfig({
   projectId: 'e1h3j61w',
   dataset: 'production',
   title: '5gmobile Admin',
+  plugins: [structureTool()],
   schema: { types: schema.types },
 })
